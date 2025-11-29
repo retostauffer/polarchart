c89833a3d84b2a850158ffc5e19c20ba34d2d5a0
Reto Stauffer <Reto.Stauffer@uibk.ac.at>
2025-11-29 15:14:47 +0100

    fix: Working on spine plot

--- polarchart/plottypes.py
+++ polarchart/plottypes.py
@@ -1,17 +1,48 @@
 
 
+import re
 from .radar import radar
 from functools import wraps
 
 def copy_doc(from_func):
+    """
+    Decorator that copies the docstring from `source`.
+    """
     def decorator(func):
         func.__doc__ = from_func.__doc__
         return func
     return decorator
 
+def copy_doc_without_examples(source):
+    """
+    Decorator that copies the docstring from `source` but removes any
+    section starting with 'Examples' (case insensitive).
+    """
+    def decorator(func):
+        doc = source.__doc__ or ""
+        # Remove "Examples" section until end of docstring
+        cleaned = re.sub(
+            r"(?is)\nExamples?:.*",   # match "Examples:" and everything after
+            "",
+            doc
+        ).rstrip()
+
+        func.__doc__ = cleaned
+        return func
+    return decorator
+
+
+###@copy_doc_without_examples(radar)
 @copy_doc(radar)
 def stars(*args, **kwargs):
     return radar(*args, _type = "stars", **kwargs)
+
+###stars.__doc__ += """
+###Examples
+###--------
+###
+###>>> 1:5
+###"""
 
 @copy_doc(radar)
 def spider(*args, **kwargs):
--- polarchart/spineplot.py
+++ polarchart/spineplot.py
@@ -6,17 +6,22 @@ import matplotlib.pyplot as plt
 from statsmodels.graphics.mosaicplot import mosaic
 from matplotlib import colors as mcolors
 
-def spineplot(df, x, y, labels = None, ax = None, *args, **kwargs):
+def spineplot(df, x, y, labels = None, ax = None, colors = None, *args, **kwargs):
     """Create spine plot
 
     Args:
         df (pandas.core.frame.DataFrame): A pandas DataFrame.
         x (str): Name of the variable for the first dimension.
-        y (str): Name of the variable for the second dimension.
+        y (str): Name of the variable for the second dimension. Must be
+            categorical or easily convertable to categorical (i.e.,
+            strings or integers with less than 10 unique values).
         labels (None, str): Either `None` (default) to suppress labels,
             or one of `"absolute"`, `"relative"`, `"percent"`.
         ax (None or matplotlib.axes._axes.Axes): If None, a new figure is
             initialized. Else the existing axis is taken, manipulated, and populated.
+        colors (None or list): If `None` (default) a series of grayscale colors
+            is used. Can be a list of colors which must match the number of
+            categories in y.
         *args:
             Positional arguments; used by other functions interfacing this main
             function (`stars()`, ...).
@@ -32,17 +37,50 @@ def spineplot(df, x, y, labels = None, ax = None, *args, **kwargs):
 
     Details:
 
-        TODO
         Allowed additional arguments via the named **kwargs:
-        - "flipud" (bool): Flip y axis upside down (default is True).
         - "title" (str): Plot title
-        - "angle" (int, float): Rotation angle in degrees.
         - "figsize" (tuple): Custom figure size, ignored if an axis ('ax') is provided.
+        - "flipud" (bool): Flip y axis upside down (default is False).
 
     Examples:
 
-    >>> # TODO
-    >>> 1 + 5
+        >>> from numpy import repeat
+        >>> from pandas import DataFrame, Series
+        >>> from polarchart import spineplot
+        >>> import matplotlib.pyplot as plt
+
+        Treatment and improvement of patients with rheumatoid arthritis
+        >>> treatment = Series(repeat([1, 2], [43, 41])).map({1: "placebo", 2: "treated"}) 
+        >>> improved  = Series(repeat([1, 2, 3, 1, 2, 3], [29, 7, 7, 13, 7, 21])).map({1: "none", 2: "some", 3: "marked"})
+        >>>
+        >>> df = DataFrame({"treatment": treatment, "improved": improved})
+        >>>
+        >>> ## Convert to categorial data and manually re-order the levels
+        >>> df.improved = df.improved.astype("category")
+        >>> df.improved = df.improved.cat.reorder_categories(["marked", "some", "none"])
+        >>>
+        >>> spineplot(df, x = "treatment", y = "improved",
+        >>>           title = "Rheumatoid arthritis treatment")
+
+        NASA space shuttle o-ring failures
+        >>> fail = Series([2, 2, 2, 2, 1, 1, 1, 1, 1, 1, 2, 1,
+        >>>                2, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1])
+        >>> fail = fail.map({1: "no", 2: "yes"})
+        >>> temperature = [53, 57, 58, 63, 66, 67, 67, 67, 68, 69, 70, 70,
+        >>>                70, 70, 72, 73, 75, 75, 76, 76, 78, 79, 81]
+        >>> df   = DataFrame({"fail": Series(fail).astype("category"),
+        >>>                   "temperature": temperature})
+        >>> df.fail = df.fail.cat.reorder_categories(["yes", "no"])
+        >>>
+        >>> spineplot(df, "temperature", "fail", figsize = (7, 3))
+        >>>
+        >>> ## Custom figure/axis and customized spine plot
+        >>> fig, ax = plt.subplots(figsize = (7, 3))
+        >>> spineplot(df, x = "temperature", y = "fail", ax = ax,
+        >>>           title = "NASA space shuttle o-ring failures",
+        >>>           colors = ["#21CCAD", "#F99BAE"])
+        >>> plt.show()
+
 
     """
 
@@ -63,6 +101,8 @@ def spineplot(df, x, y, labels = None, ax = None, *args, **kwargs):
         raise TypeError("argument 'labels' must be None or str")
     if not isinstance(ax, (axes._axes.Axes, type(None))):
         raise TypeError("argument 'ax' must be None or matplotlib.axes._axes.Axes")
+    if not isinstance(colors, (type(colors), list)):
+        raise TypeError("argument 'colors' must be None or list")
 
     if not x in df.columns:
         raise ValueError(f"variable {x=} not found in 'df'")
@@ -85,25 +125,11 @@ def spineplot(df, x, y, labels = None, ax = None, *args, **kwargs):
     if "flipud" in kwargs:
         if not isinstance(kwargs["flipud"], bool):
             raise TypeError("**kwarg 'flipud' must be bool")
-    flipud = True if not "flipud" in kwargs else kwargs["flipud"]
-
-    # If x/y are integer, check if they are 'continuous'. If so,
-    # we handle them as integers. Else we use 'cut'.
-    def column_to_category(x):
-        # Integer sequence?
-        if np.issubdtype(x, int):
-            tmp = np.asarray([y for y in range(min(x), max(x) + 1)])
-            if np.all(tmp == np.intersect1d(tmp, x)):
-                return x.astype("category"), None
-
-        if np.issubdtype(x, float):
-            bins = Sturges(x)
-            x = pd.cut(x, bins = bins)
-        return x, bins
+    flipud = False if not "flipud" in kwargs else kwargs["flipud"]
 
     # Convert original data to categorical
     x_cat, x_bins = column_to_category(df[x])
-    y_cat, y_bins = column_to_category(df[y])
+    y_cat, y_bins = column_to_category(df[y], nmax = 10)
 
     # Create new data.frame with only categorical data
     df = pd.DataFrame(dict(x = x_cat, y = y_cat))
@@ -118,17 +144,13 @@ def spineplot(df, x, y, labels = None, ax = None, *args, **kwargs):
     tab.columns = tab.columns.astype("str")
 
     # Drawing color lookup dictionary
-    n_cols   = len(df.y.values.categories)
-    hex_cols = [mcolors.to_hex(x) for x in plt.cm.gray(np.linspace(0.9, .3, n_cols))]
-    cols = dict()
-    for i,k in enumerate(df.y.values.categories):
-        cols[str(k)] = hex_cols[i]
-    del hex_cols, n_cols
+    cols = get_spineplot_colors(df.y, colors, flipud)
 
-    # Color coding
-    # Scopes 'cols'
+    # Returns properties for facets; scopes 'cols'
     def colors(key):
-        return {"color": cols[key[1]]}
+        return {"facecolor": cols[key[1]],
+                "edgecolor": "black",
+                "linewidth": 0.5}
         #return {"color": cols[int(key[1]) - 1]}
 
     # Scoping 'tab' and 'labels'
@@ -141,30 +163,42 @@ def spineplot(df, x, y, labels = None, ax = None, *args, **kwargs):
             case "relative":    return(x / n)
             case _:             return(x)
 
+    # Gap 0.02 to ~0.01
+    gap = 0 if x_bins is not None else 0.01 + 0.01 / df.x.nunique()
+
+    # Create main plot
     mosaic(tab.iloc[:,::-1 if flipud else 1].stack(), ax = ax,
-           properties = colors, gap = (0.02, 0),
+           properties = colors, gap = (gap, 0),
            statistic = False, labelizer = labelizer)
 
     # I updating axis
     if x_bins is not None:
-        ax.set_xticks([0.25, 0.75])
-        ax.set_xticklabels(['Left', 'Right'])
+        at = np.concatenate([np.asarray([0]), tab.apply("sum", 1).cumsum().to_numpy()])
+        at = at / np.max(at)
+        ax.set_xticks(at)
+        ax.set_xticklabels([f"{x:.2f}" for x in x_bins])
+
     if y_bins is not None:
-        at = np.cumsum((tab.iloc[0, :] / sum(tab.iloc[0, :])).values)
-        at = np.concatenate([np.asarray([0]), at])
-        print(y_bins)
-        print(at)
+        at = np.concatenate([np.asarray([0]), tab.apply("sum", 0).cumsum().to_numpy()])
+        at = at / np.max(at)
         ax.set_yticks(at)
         ax.set_yticklabels([f"{x:.2f}" for x in y_bins])
+
+    # Adding title if requested
+    if "title" in kwargs: ax.set_title(kwargs["title"])
+
 
     # If 'fig = None' the user provided their own axis ('ax = ...'),
     # in this case we just return the axis. Else we show the plot.
     if fig is None:
         return ax
     else:
+        plt.show()
         return df, tab
 
 
+# -------------------------------------------------------------------
+# -------------------------------------------------------------------
 def Sturges(x):
     """Calculates Sturges breaks on a numeric variable
 
@@ -181,6 +215,99 @@ def Sturges(x):
     n = int(np.ceil(np.log2(len(x)) + 1))
     return np.linspace(start = np.min(x), stop = np.max(x), num = n)
 
+
+# -------------------------------------------------------------------
+# -------------------------------------------------------------------
+# If x/y are integer, check if they are 'continuous'. If so,
+# we handle them as integers. Else we use 'cut'.
+def column_to_category(x, nmax = None):
+    """
+    Converting pandas series to categorical.
+
+    Args:
+        x (pandas.core.series.Series): Can be of any type, this function tries to convert
+            it to categorical used to calculate the crosstable, the main
+            object the spine plot is based on.
+        nmax (None, int): An integer can be specified to force numeric series with
+            many unique values not to be handled as categorical (typically if you
+            have integers 1, 2, 3, 4) but to be handled as 'continuous' cut
+            into intervals (as is done by default for floats).
+
+    Return:
+        pandas.core.series.Series: Returns categorical series.
+    """
+
+    from pandas import Series
+    if not isinstance(x, Series):
+        raise TypeError("argument 'x' must be pandas.core.series.Series")
+    if not isinstance(nmax, (type(None), int)):
+        raise TypeError("argument 'nmax' must be None or int")
+    if isinstance(nmax, int) and nmax < 1:
+        raise ValueError("argument 'nmax' (int) cannot be <= 0")
+
+    assert isinstance(nmax, (type(None), int)), "nmax must be None or int"
+
+    # If already categorical, return. If object, convert to category and return.
+    if x.dtype.name == "category": return x, None
+    if x.dtype.name == "object":   return x.astype("category"), None
+
+    # Integer sequence?
+    if np.issubdtype(x, int):
+        tmp = np.asarray([y for y in range(min(x), max(x) + 1)])
+        ## If all values 'tmp' are in x and the number of unique
+        ## integers is < 20 we assume it is 'categorical'.
+        if x.nunique() < 20 and np.all(np.isin(tmp, x)):
+            return x.astype("category"), None
+
+    # Convert numeric variable (used for x-axis) to categories
+    # using pandas.cut() if 'x' is numeric and nmax is None.
+    if np.issubdtype(x, (int, float)) and nmax is None:
+        bins = Sturges(x)
+        x = pd.cut(x, bins = bins)
+    # Else we try to take all unique values of the vector
+    # as categories, but will throw an error if there are
+    # more than 'nmax'.
+    else:
+        nx = x.nunique()
+        if nx > nmax:
+            raise ValueError(f"problem converting to categorical variable, more unique values than allowed ({nx} > {nmax}).")
+        x = x.astype("category")
+        bins = None
+
+    return x, bins
+
+# -------------------------------------------------------------------
+# -------------------------------------------------------------------
+def get_spineplot_colors(y, colors, flipud):
+    """
+    Get color vector for spine plot. If `colors` is `None` we pick
+    a series of grayscale colors. Else `colors` is a list, we check if
+    this has correct length (TODO: Check if valid colors?)
+
+    Args:
+        y (pandas.Series): Our categorical variable
+        colors (None, list): Colors to be used.
+        flipud (bool): Wether or not the colors should be inverted.
+
+    Return:
+        list: List of length `n` with the colors used for the facets.
+    """
+    n = y.nunique()
+
+    if colors is None:
+        colors = [mcolors.to_hex(x) for x in plt.cm.gray(np.linspace(0.95, .3, n))]
+    else:
+        if not len(colors) == n:
+            raise ValueError(f"length of 'colors' does not match number of required colors (n = {n}")
+
+    # Prepare dictionary for coloring the facets
+    cols = dict()
+    if not flipud:
+        for i,k in enumerate(reversed(y.values.categories)): cols[str(k)] = colors[i]
+    else:
+        for i,k in enumerate(y.values.categories): cols[str(k)] = colors[i]
+
+    return cols
 
 
 
